924e3a8794a9d09da8a393f6f1730ef01353cc8f
edison-xiao <381004367@qq.com>
2016-09-09 01:03:20 +0800

    新建项目

---
+++ app/src/androidTest/java/greendao/test/com/testgradle/ApplicationTest.java
@@ -0,0 +1,13 @@
+package greendao.test.com.testgradle;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
